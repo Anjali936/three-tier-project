c3397fcf9fa096ff7d60aceaa4d06cf5fe2f3bdc
Anjali admin1 <anjali.saindane95@gmail.com>
2025-05-22 14:10:39 +0200

    this is my code

---
+++ aws-three-tier-web-app/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.ct48ocmmm4jd.eu-central-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin123456',
+    DB_DATABASE : 'appdb'
+});
